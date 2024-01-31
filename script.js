2b0a5fd5fdf2a2b580f6b09c953048e30dd284d9
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-01-31 13:01:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const firstDate = new Date(date1);
+    const secondDate = new Date(date2);
+
+    // Calculate the difference in milliseconds
+    const diffMilliseconds = secondDate - firstDate;
+
+    // Calculate the difference in days
+    const diffDays = Math.round(diffMilliseconds / oneDay);
+
+    return diffDays;
 };
 
 // Do not change the code below.
